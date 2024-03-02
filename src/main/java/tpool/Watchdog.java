6d159818efeca1ff3afca9fc1f6be5c7cebd7994
PUSHRSP <pushrsp@gmail.com>
2024-03-02 13:22:01 +0900

    Watchdog 추가

---
+++ src/main/java/tpool/Watchdog.java
@@ -0,0 +1,55 @@
+package tpool;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+final class Watchdog extends AbstractWorker {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Watchdog.class);
+
+    private final TPool tPool;
+    private final long taskTimeoutNanos;
+    private final long watchdogIntervalMillis;
+    private final int watchdogIntervalRemainingNanos;
+
+    Watchdog(TPool tPool, long taskTimeoutNanos, long watchdogIntervalNanos) {
+        super(WorkerType.WATCHDOG, "Watchdog");
+        this.tPool = tPool;
+        this.taskTimeoutNanos = taskTimeoutNanos;
+
+        final long nanosPerMilli = TimeUnit.MILLISECONDS.toNanos(1); // 1,000,000
+        this.watchdogIntervalMillis = watchdogIntervalNanos / nanosPerMilli;
+        this.watchdogIntervalRemainingNanos = (int) (watchdogIntervalNanos % nanosPerMilli);
+    }
+
+    @Override
+    void go() {
+        LOGGER.debug("Started a watchdog {}", workerName());
+        try {
+            while (!tPool.isShutdown()) {
+                try {
+                    sleep(watchdogIntervalMillis, watchdogIntervalRemainingNanos);
+                } catch (InterruptedException ignore) {
+                    continue;
+                }
+
+                tPool.forEach(w -> {
+                    final long taskStartTimeNanos = w.taskStartTimeNanos();
+                    if (taskStartTimeNanos == TPool.TASK_NOT_STARTED_MARKER) {
+                        return;
+                    }
+
+                    if (System.nanoTime() - taskStartTimeNanos > taskTimeoutNanos) {
+                        w.interrupt(TerminationReason.WATCHDOG);
+                    }
+                });
+            }
+        } catch (Throwable cause) {
+            LOGGER.warn("Unexpected exception from a watchdog: {}", terminationReason());
+        } finally {
+            LOGGER.debug("{} has been terminated", workerName());
+        }
+    }
+}
